758c7a9830a2bea68c5e00c911d73933dbe00649
yannhallage <contact.devhllg@gmail.com>
2025-09-09 22:52:51 +0000

    developpment des interfaces en fonction des fonctionnalités

--- src/secure/ProtectedRoute.tsx
+++ src/secure/ProtectedRoute.tsx
@@ -1,5 +1,17 @@
 import { getSession } from "@/lib/localstorage";
+import { jwtDecode } from 'jwt-decode';
 import { Navigate } from "react-router-dom";
+
+
+const isTokenValid = (token: string): boolean => {
+    try {
+        const decoded: any = jwtDecode(token);
+        const now = Date.now() / 1000;
+        return decoded.exp > now;
+    } catch {
+        return false;
+    }
+};
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
     const session = getSession();
@@ -7,7 +19,6 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
     if (!session) {
         return <Navigate to="/auth" replace />;
     }
-
     return <>{children}</>;
 };
 
